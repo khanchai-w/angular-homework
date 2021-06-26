1f4ef546c1523071fd66199cdc511e19acdcd0c7
khanchai-w <hs3wmn@gmail.com>
2021-06-26 13:07:32 +0700

    HomeWork 2

--- homework2/src/app/app.module.ts
+++ homework2/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { ToDoComponent } from './todo/todo.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
